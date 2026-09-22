c98b76301524ea29b328085299860272af7e5295
jahid2k1 <jahid2k1@gmail.com>
2026-09-22 08:01:22 +0000

    default rate 13.95

--- sw.js
+++ sw.js
@@ -2,3 +2,13 @@ const CACHE="cashout-calc-v1";const ASSETS=["./","./index.html","./manifest.json
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+const DEFAULT_RATE = 13.95;
+const RATE_VERSION = "2 ";
+
+if (localStorage.getItem("rateVersion") !== RATE_VERSION) {
+    localStorage.setItem("rate", DEFAULT_RATE);
+    localStorage.setItem("rateVersion", RATE_VERSION);
+}
+
+const rate = Number(localStorage.getItem("rate"));
